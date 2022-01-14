44c9f3d6a14e3fbb69eafbbb5cd36659937fcfd8
iirtrtrt <iirtrtrt@gmail.com>
2022-01-14 10:24:05 +0100

    the last

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingController.java
@@ -1,12 +1,17 @@
 package org.springframework.samples.petclinic.feeding;
 
+import java.util.Collection;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.Pet;
+import org.springframework.samples.petclinic.pet.PetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -14,10 +19,22 @@ public class FeedingController {
     private static final String createOrUpdateFeedingForm = "feedings/createOrUpdateFeedingForm";
 
     private final FeedingService feedingService;
+    private final PetService petService;
 
     @Autowired
-    public FeedingController(FeedingService feedingService) {
+    public FeedingController(FeedingService feedingService, PetService petService) {
         this.feedingService = feedingService;
+        this.petService = petService;
+    }
+
+    @ModelAttribute("pets")
+    public Collection<Pet> populatePetTypes() {
+        return petService.getAllPets();
+    }
+
+    @ModelAttribute("feedingTypes")
+    public Collection<FeedingType> populateFeedingTypes() {
+        return feedingService.getAllFeedingTypes();
     }
 
     @GetMapping(value = "/feeding/create")
